7f6858ca2c35e588dd2975b02d2a97720e3db2b4
Zabdiel Anyaogu <zabdiel@resultapp.org>
2026-09-18 18:46:03 +0100

    feat: add Footer component for school landing page with navigation and contact info

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -74,7 +74,7 @@ export default function Footer({ school, subdomain }: FooterProps) {
             <li>
               <Link
                 href={`/${subdomain}/staff`}
-                className="text-purple-200/70 transition hover:text-white"
+                className="inline-flex min-h-[44px] items-center text-purple-200/70 transition hover:text-white"
               >
                 Staff Portal
               </Link>
