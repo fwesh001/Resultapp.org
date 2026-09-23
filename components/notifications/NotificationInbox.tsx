95ca144c077368ee3777e328dda39133ca3711d9
ANYAOGU .C. ZABDIEL <160980730+fwesh001@users.noreply.github.com>
2026-09-23 16:21:02 +0100

    feat: add NotificationInbox component for tenant notification management

--- components/notifications/NotificationInbox.tsx
+++ components/notifications/NotificationInbox.tsx
@@ -374,7 +374,7 @@ export default function NotificationInbox({ tenantId, basePath }: NotificationIn
                         </button>
                       </div>
                     )}
-                  </ul>
+                  </li>
                 );
               })}
             </ul>
